e64b673d4f19eaab400e2aeb0847cdcaf9de0c88
Willamina (Jitera) <developers@jitera.com>
2024-01-11 03:45:04 +0000

    Update code

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 require 'sidekiq/web'
+
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+  get '/api/notes', to: 'api/notes#index', as: 'list_user_notes'
   put '/api/notes/:id/autosave', to: 'notes#autosave'
   get '/health' => 'pages#health_check'
   get 'api-docs/v1/swagger.yaml' => 'swagger#yaml'
